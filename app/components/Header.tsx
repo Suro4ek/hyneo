3253b219f29c80fc135888b122599e506659fb5e
1Suro1 <timu.zaxarov@gmail.com>
2022-07-16 18:14:27 +0900

    edit

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,11 +9,11 @@ const Header = () => {
         <header className={"w-full h-screen "}>
             <nav className="container mx-auto">
                 <div className="relative flex justify-center items-center [&>*]:px-4 pt-8 text-lg text-white">
-                    <a className="flex"><BsShop className="w-6 h-6 mr-2" />Магазин</a>
-                    <a className="flex"><IoNewspaperOutline className="w-6 h-6 mr-2" /> Правила</a>
-                    <a className="flex"><GiHammerDrop className="w-6 h-6 mr-2" />Банлист</a>
-                    <a className="flex"><GiHelp className="w-6 h-6 mr-2" />Помощь</a>
-                    <a className="flex"><TiSocialFacebook className="w-6 h-6 mr-2" />Группа ВК</a>
+                    <a className="inline-flex items-center mr-2 mb-2"><BsShop className="w-6 h-6 mr-2 -ml-1" />Магазин</a>
+                    <a className="inline-flex items-center mr-2 mb-2"><IoNewspaperOutline className="w-6 h-6 mr-2 -ml-1" /> Правила</a>
+                    <a className="inline-flex items-center mr-2 mb-2"><GiHammerDrop className="w-6 h-6 mr-2 -ml-1" />Банлист</a>
+                    <a className="inline-flex items-center mr-2 mb-2"><GiHelp className="w-6 h-6 mr-2 -ml-1" />Помощь</a>
+                    <a className="inline-flex items-center mr-2 mb-2"><TiSocialFacebook className="w-6 h-6 mr-2 -ml-1" />Группа ВК</a>
                 </div>
             </nav>
             <main>
@@ -32,16 +32,19 @@ const Header = () => {
                                     <br />
 
                                     + с огромным функционалом, а также возможность запускать свой собственный сервер!
-                                    <br/>
+                                    <br />
                                     Сервер работает с версии ........
                                     <div className="pt-8">
-                                            IP mc.hyneo.ru
+                                        IP mc.hyneo.ru
                                     </div>
                                 </div>
                             </div>
-                            <div className="mt-4 flex">
-                                <button>Начать играть</button>
-                                <button>Донат</button>
+                            <div className="mt-4 flex text-2xl">
+                                <button className="bg-[#ffa500] rounded-[50px] px-16 h-16 mx-4 transition ease-in-out duration-[1000ms] hover:shadow-[0_0px_2rem_-1px_#ffc55b] hover:bg-[#fdba40]">
+                                    Начать играть
+                                </button>
+                                <button className="bg-[#ffa500] rounded-[50px] px-10 mx-4 transition ease-in-out duration-[1000ms] hover:shadow-[0_0px_2rem_-1px_#ffc55b] hover:bg-[#fdba40]"
+                                >Донат</button>
                             </div>
                         </div>
                     </div>
