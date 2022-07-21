75d6d929dfc6af169f742a66bb8df0557d1ca74a
1Suro1 <timu.zaxarov@gmail.com>
2022-07-21 13:45:36 +0900

    test

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,30 +1,30 @@
 import Header from "~/components/Header";
-import Shop from "~/components/Shop";
-import TLauncherModal from "~/components/TLauncherModal";
-import BuyModal from "~/components/BuyModal";
-import { LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { getCategoriesByItems } from "~/models/category.server";
-import { getMedthods } from "~/models/method.server";
-import { RecoilRoot } from "recoil";
+// import Shop from "~/components/Shop";
+// import TLauncherModal from "~/components/TLauncherModal";
+// import BuyModal from "~/components/BuyModal";
+// import { LoaderFunction } from "@remix-run/node";
+// import { useLoaderData } from "@remix-run/react";
+// import { getCategoriesByItems } from "~/models/category.server";
+// import { getMedthods } from "~/models/method.server";
+// import { RecoilRoot } from "recoil";
 
-export const loader: LoaderFunction = async () => {
-    const categories = await getCategoriesByItems();
-    const methods = await getMedthods();
-    return { categories, methods };
-  };
+// export const loader: LoaderFunction = async () => {
+//     const categories = await getCategoriesByItems();
+//     const methods = await getMedthods();
+//     return { categories, methods };
+//   };
 
 export default function Index() {
-    const data = useLoaderData();
+    // const data = useLoaderData();
 
     return (
-        <RecoilRoot>
+        <>
             <Header />
             {/* <Shop/> */}
             {/* <Footer/> */}
             {/* <TLauncherModal />
             <BuyModal /> */}
-        </RecoilRoot>
+        </>
 
     );
 }
